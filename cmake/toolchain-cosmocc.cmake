724fff627660ce03f6ba344f20e37d7763a806f7
Araf Al Jami <clown1331@gmail.com>
2023-11-12 18:58:20 +0000

    Remove cosmo broken

--- cmake/toolchain-cosmocc.cmake
+++
@@ -1,8 +0,0 @@
-if(CMAKE_GENERATOR MATCHES "Visual Studio")
-    message(FATAL_ERROR "Visual Studio generator not supported, use: cmake -G Ninja")
-endif()
-set(CMAKE_SYSTEM_NAME "Linux")
-set(CMAKE_SYSTEM_VERSION 1)
-set(CMAKE_SYSTEM_PROCESSOR "X86")
-set(CMAKE_C_COMPILER "/opt/cosmo/bin/cosmocc")
-set(CMAKE_CXX_COMPILER "/opt/cosmo/bin/cosmoc++")
